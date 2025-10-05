7ea16176462c7a2f13d6199a1736571ec5222f77
Moises Lara <laramoises73@gmail.com>
2025-10-05 17:09:29 -0600

    push components proyect base

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,10 @@
-import "./style.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+
+import Header from "./components/Header/header.html?raw";
+import Footer from "./components/Footer/footer.html?raw";
 
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
-  
+${Header}
+${Footer}
 `;
